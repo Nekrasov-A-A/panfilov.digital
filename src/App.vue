99b6a49a859b1928570e48a1721c6950821096fc
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-09-22 08:05:44 +0300

    feat(component): add transition for views

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div id="app" class="container">
+    <transition name="move" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
@@ -11,3 +13,23 @@ export default {
   name: 'App',
 };
 </script>
+
+<style>
+.container {
+  overflow-x: hidden;
+}
+.move-enter-from, .move-leave-to {
+  transform: translateX(-100%);
+  opacity: 0;
+}
+.move-enter-to, .move-leave-from  {
+  transform: translateX(0);
+  opacity: 1;
+}
+.move-enter-active {
+  transition: all .5s;
+}
+.move-leave-active {
+  transition: all .3s;
+}
+</style>
